616b7766af31a5880b29b5f9ed1419724b9aafeb
gokul85 <gokulkumar5505@gmail.com>
2023-12-17 20:07:51 +0530

    Final Commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
